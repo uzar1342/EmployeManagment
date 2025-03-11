9efe61fbc60f1300761e1c9b89c553b6ab6531a7
arbaz <uzar1342@gmail.com>
2025-03-12 01:14:19 +0530

    commit 1

--- server.js
+++ server.js
@@ -7,8 +7,11 @@ const userRoutes = require('./routes/userRoutes');
 
 const app = express();
 app.use(express.json());
-app.use(cors());
-
+app.use(cors({
+    origin: '*',
+    methods: ['GET', 'POST', 'PUT', 'DELETE'],
+    credentials: true
+  }));
 app.use('/api/auth', authRoutes);
 app.use('/api/employees', employeeRoutes);
 app.use('/api/users', userRoutes);
