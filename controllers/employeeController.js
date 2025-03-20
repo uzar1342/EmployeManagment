3f6f10158249d61c592492fea0c82619b415b997
arbaz <uzar1342@gmail.com>
2025-03-20 16:40:17 +0530

    emp name

--- controllers/employeeController.js
+++ controllers/employeeController.js
@@ -3,6 +3,13 @@ const Employee = require('../models/Employee');
 exports.createEmployee = async (req, res) => {
   try {
     const { name, position, salary } = req.body;
+
+    // Validation: Check if the same user already has an employee with the same name
+    const existingEmployee = await Employee.findOne({ name, createdBy: req.user.id });
+    if (existingEmployee) {
+      return res.status(400).send('You already have an employee with this name'); // 400 Bad Request
+    }
+
     const employee = new Employee({ name, position, salary, createdBy: req.user.id });
     await employee.save();
     res.status(201).send('Employee created successfully'); // 201 Created
@@ -41,6 +48,12 @@ exports.updateEmployee = async (req, res) => {
     // Only Admin or the creator can update
     if (req.user.role !== 'Admin' && employee.createdBy.toString() !== req.user.id)
       return res.status(403).send('Access denied'); // 403 Forbidden
+
+    // Validation: Check if the same user already has another employee with the same name
+    const existingEmployee = await Employee.findOne({ name, createdBy: req.user.id, _id: { $ne: id } });
+    if (existingEmployee) {
+      return res.status(400).send('You already have an employee with this name'); // 400 Bad Request
+    }
 
     employee.name = name;
     employee.position = position;
